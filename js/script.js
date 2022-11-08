4b12e16476b7afc2d9072d8dd518c13d02a57714
jhock9 <jon.hocker@gmail.com>
2022-11-08 12:04:39 -0600

    first api call script

--- js/script.js
+++ js/script.js
@@ -2,42 +2,37 @@ require('dotenv').config()
 const API_URL = process.env.API_URL;
 const API_KEY = process.env.API_KEY;
 
-
 const allImages = document.querySelector(".images-container");
 const newImagesButton = document.querySelector("#next");
 
-// ? parameter "could" be passed based on filters
-const getImages = async function () {
-  const fetchAirplanes = await fetch('${API_URL}?key=${API_KEY}&q=airplane');
-  const airplanes = await fetchAirplanes.json();
-  console.log(airplanes);
-  
+// AIRPLANES
+const getAirplanes = async function () {
+  const fetchAirplanes = await fetch('${API_URL}?key=${API_KEY}&q=airplane&category=transportation&per_page=8&page=1');
+  const airplanesData = await fetchAirplaneData.json();
 
+  const fetchAirplaneImages = await fetch (airplanesData.webformatURL);
+  const airplaneImages = await fetchAirplaneImages.json();
 
-// ? adding the fetched images to an array
-  // const imageArray = [];
-  // imageArray.length = 8;
-  // for (const img in imageData) {
-  //   imageArray.push(img);
-  // };
-  
-  // displayImages(airplanes);
+  const airplanes = [];
+  for (const airplane in airplaneImages) {
+    airplanes.push(airplane);
+  };
+  // TODO randomize array
 
-  // pull images from api -- based on filter parameters
-  // put into an array of 8 -- randomized
-  // at least 3 images need to be the same type / have same tag
+  displayImages(airplanes);
+
+  // TODO random merge with another array, total of 8 (3 % 5)
+
   // 
 };
 
-// const displayImages = function (image) {
-//   for (const image of images) {
-//     const div = document.createElement("div");
-//     div.classList.add("image");
-//     div.innerHTML = 
-//       <img src=${_____} alt=${_____}/>;
-//     allImages.append(div);      
-//   };
-// };   
+const displayImages = function (airplanes) {
+  const div = document.createElement("div");
+  div.classList.add("image");
+  div.innerHTML = 
+    <img src=${_____} alt=${_____}/>;
+  allImages.append(div);      
+};   
 
 
 // newImagesButton.addEventListener("click", function () {
