724940585205f0702b0a9a4441682eab5c5c8904
jhock9 <jon.hocker@gmail.com>
2022-11-10 16:52:32 -0600

    small tweak to code

--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@ const getAirplanes = async function () {
 };
 getAirplanes();
 
-// Randomizes images displayed
+// Randomizes images displayed and shortens array length
 const shuffleImages = function (array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
