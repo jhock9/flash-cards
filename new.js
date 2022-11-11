168594a6229b05067ad711744218e1ffdffb9967
jhock9 <jon.hocker@gmail.com>
2022-11-11 14:21:06 -0600

    removed random file

--- new.js
+++
@@ -1,9 +0,0 @@
-var isBrowser=new Function("try {return this===window;}catch(e){ return false;}");
-
-// tests if global scope is bound to window
-if(isBrowser()) console.log("running under browser");
-
-var isNode=new Function("try {return this===global;}catch(e){return false;}");
-
-// tests if global scope is bound to "global"
-if(isNode()) console.log("running under node.js");
