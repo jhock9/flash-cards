121ec7515f5885b11e7d2bbc2a568384221b8904
Jon Hocker <jon.hocker@gmail.com>
2023-02-03 16:13:24 -0600

    bug fix

--- server/server.js
+++ server/server.js
@@ -3,16 +3,16 @@ const app = express();
 const port = process.env.PORT || 3003;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '../index.html');
 });
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '../'));
 
 app.listen(port, () => {
   console.log(`Server running at http://test.jonhocker.com:${port}`);
 });
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  console.error(err);
   res.status(500).send('Something went wrong!');
 });
