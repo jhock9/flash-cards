f6e1327e9024d1aa9d6f9bdbc398af0ab89e21ce
Jon Hocker <jon.hocker@gmail.com>
2023-02-14 09:25:31 -0600

    bug check

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get('/bundle.js', (req, res) => {
 });
 
 // Serve static files
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../src')));
 
 // Log serving file
 app.use((req, res, next) => {
