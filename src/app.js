77759cd9d864b21ef1543f6424a556e694d9148a
Jon Hocker <jon.hocker@gmail.com>
2023-02-15 15:19:28 -0600

    testing sign in with apple

--- src/app.js
+++ src/app.js
@@ -2,10 +2,9 @@
 // require('./main.css');
 // if (typeof window !== 'undefined' && window.process && window.process.env && window.process.env.NODE_ENV === 'production') {
 
-
 const flashCardPage = document.querySelector("#flash-card-page");
 const landingPage = document.querySelector("#landing-page");
-const signInBtn = document.querySelector("#apple-signin-btn");
+const signInBtn = document.querySelector("#apple-sign-in-button");// need this still??
 const submit = document.querySelector('#submit-btn');
 const filterList = document.querySelector('.filter-list');
 const filterInputs = Array.from(document.getElementsByClassName('qty'));
@@ -13,49 +12,145 @@ const allImages = document.querySelector('.images-container');
 let currentAlbums = [];
 let userIdentity = null;
 
+console.log(process.env.NODE_ENV);
+console.log(process.env.ICLOUD_CONTAINER);
+console.log(process.env.ICLOUD_API_KEY);
+console.log(process.env.ICLOUD_REDIRECT_URI);
+
 //* CONFIGURING CLOUDKIT & ICLOUD USER AUTHENTICATION
-CloudKit.configure ({
-  containers: [{
-    containerId: process.env.ICLOUD_CONTAINER,
-    apiTokenAuth: {
-        apiToken: process.env.ICLOUD_API_KEY,
-        persist: true, // keeps user signed in after closing/reopening browser
-        useAuth: true
-    },
-    environment: process.env.NODE_ENV === 'production' ? 'production' : 'development'
-  }]
-});
+// window.addEventListener('cloudkitloaded', () => {
+  console.log('listening for cloudkitloaded');
+  CloudKit.configure ({
+    containers: [{
+      containerIdentifier: process.env.ICLOUD_CONTAINER,
+      apiTokenAuth: {
+          apiToken: process.env.ICLOUD_API_KEY,
+          persist: true, // keeps user signed in after closing/reopening browser
+          // useAuth: true
+          signInButton: {
+            id: 'apple-sign-in-button',
+            theme: 'black' // Other options: 'white', 'white-with-outline'.
+          },
+        },
+      environment: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+    }]
+  });
+  console.log('cloudkitloaded');
 
-CloudKit.on('error', (error) => {
-  console.error(error);
-});
+  let container = CloudKit.getDefaultContainer();
+  container.setUpAuth().then((userInfo) => {
+    if(userInfo) {
+      // this.gotoAuthenticatedState(userInfo);
+      console.log(userInfo + " " + "is authenticated.");
+    } else {
+      // this.gotoUnauthenticatedState();
+      console.log(userInfo + " " + "is not authenticated.");
+    }
+  });
 
-CloudKit.getAuthStatus().then(function(response) {
-  if(response.status === 'AUTHORIZED') {
-    console.log('User is already signed in');
-    landingPage.classList.add("hide");
-    flashCardPage.classList.remove("hide");
-    fetchAlbums();
-  } else {
-    console.log('User is not signed in');
-    signInBtn.addEventListener("click", () => {
-      CloudKit.signIn({
-        scope: 'email',
-        redirectURI: process.env.ICLOUD_REDIRECT_URI
-      }).then((response) => {
-        console.log(response);
-        if(response.isSuccess) {
-          landingPage.classList.add("hide");
-          flashCardPage.classList.remove("hide");
-          userIdentity = response.userIdentity;
-          fetchAlbums();
-        } else {
-          console.error('Error signing in:', response.error);
-        };
-      });
-    });
-  };
-});
+  container.whenUserSignsIn().then((userInfo) => {
+    console.log(userInfo + " " + "just signed in.");
+  });
+  container.whenUserSignsOut().then((userInfo) => {
+    console.log(userInfo + " " + "just signed out.");
+  });
+
+
+  // let TILViewModel = () => {
+  //   console.log("get default container");
+  //   let container = CloudKit.getDefaultContainer();
+    
+  //   console.log("set publicDB");
+  //   let publicDB = container.publicCloudDatabase;
+  //   this.items = ko.observableArray();
+
+  //   // Fetch public records
+  //   this.fetchRecords = () => {
+  //     console.log("fetching records from " + publicDB);
+  //     let query = { recordType: 'Acronym', sortBy: [{ fieldName: 'short'}] };
+      
+  //     // Execute the query.
+  //     return publicDB.performQuery(query).then((response) => {
+  //       if (response.hasErrors) {
+  //         console.error(response.errors[0]);
+  //         return;
+  //       }
+  //       let records = response.records;
+  //       let numberOfRecords = records.length;
+  //       if (numberOfRecords === 0) {
+  //         console.error('No matching items');
+  //         return;
+  //       }
+  //       this.items(records);
+  //     });
+  //   };
+
+  //   this.displayUserName = ko.observable('Unauthenticated User');
+  //   this.gotoAuthenticatedState = (userInfo) => {
+  //     if(userInfo.isDiscoverable) {
+  //       this.displayUserName(userInfo.firstName + ' ' + userInfo.lastName);
+  //     } else {
+  //       this.displayUserName('userInfo.userRecordName');
+  //     }
+    
+  //     container
+  //     .whenUserSignsOut()
+  //     .then(this.gotoUnauthenticatedState);
+  //   };
+    
+  //   this.gotoUnauthenticatedState = (error) => {
+  //     this.displayUserName('Unauthenticated User');
+    
+  //     container
+  //     .whenUserSignsIn()
+  //     .then(this.gotoAuthenticatedState)
+  //     .catch(this.gotoUnauthenticatedState);
+  //   };
+    
+  
+  //   container.setUpAuth().then((userInfo) => {
+  //     console.log("setUpAuth");
+  //     this.fetchRecords(); // Don't need user auth to fetch public records
+    
+  //     if (userInfo) {
+  //       this.gotoAuthenticatedState(userInfo);
+  //     } else {
+  //       this.gotoUnauthenticatedState();
+  //     }
+  //   });
+  // }
+  
+  // ko.applyBindings(new TILViewModel());
+// });
+
+
+
+// CloudKit.getAuthStatus().then((response) => {
+//   if (response.status === 'AUTHORIZED') {
+//     console.log('User is already signed in');
+//     landingPage.classList.add("hide");
+//     flashCardPage.classList.remove("hide");
+//     fetchAlbums();
+//   } else {
+//     console.log('User is not signed in');
+//     signInBtn.addEventListener("click", () => {
+//       CloudKit.signIn({
+//         scope: 'email',
+//         redirectURI: process.env.ICLOUD_REDIRECT_URI
+//       }).then((response) => {
+//         console.log(response);
+//         if (response.isSuccess) {
+//           landingPage.classList.add("hide");
+//           flashCardPage.classList.remove("hide");
+//           userIdentity = response.userIdentity;
+//           fetchAlbums();
+//         } else {
+//           console.error('Error signing in:', response.error);
+//         };
+//       });
+//     });
+//   };
+// });
 console.log(process.env.NODE_ENV);
 console.log(process.env.ICLOUD_CONTAINER);
 console.log(process.env.ICLOUD_API_KEY);
@@ -63,7 +158,7 @@ console.log(process.env.ICLOUD_REDIRECT_URI);
 
 //* CREATING KEYWORD LIST FOR USER TO CHOOSE FROM 
 // Using album names as keywords 
-function fetchAlbums() {
+const fetchAlbums = () => {
   let container = CloudKit.getDefaultContainer();
   let database = container.publicCloudDatabase;
   let albumQuery = {
@@ -76,12 +171,12 @@ function fetchAlbums() {
       }
     }],
   };
-  database.performQuery(albumQuery).then(function(response) {
-    if(response.hasErrors) {
+  database.performQuery(albumQuery).then((response) => {
+    if (response.hasErrors) {
       console.error(response.errors[0]);
     } else {
       let records = response.records;
-      for(let i = 0; i < records.length; i++) {
+      for (let i = 0; i < records.length; i++) {
         let record = records[i];
         let albumName = record.fields.name.value;
         currentAlbums.push(albumName);
@@ -129,7 +224,7 @@ submit.addEventListener('click', () => {
 });
 
 //* FETCHING, SHUFFLING & DISPLAYING PHOTOS BASED ON USER INPUT
-function fetchPhotos(albumNames, qtys) {
+const fetchPhotos = (albumNames, qtys) => {
   let container = CloudKit.getDefaultContainer();
   let database = container.publicCloudDatabase;
   let promises = [];
@@ -146,7 +241,7 @@ function fetchPhotos(albumNames, qtys) {
     };
     promises.push(database.performQuery(query));
   };
-  Promise.all(promises).then(function(responses) {
+  Promise.all(promises).then((responses) => {
     let allPhotos = [];
     for (let response of responses) {
       let records = response.records;
@@ -165,7 +260,7 @@ function fetchPhotos(albumNames, qtys) {
 };
 
 // Helper function to randomize array length based on user input
-function shuffleArray(array) {
+const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -174,7 +269,7 @@ function shuffleArray(array) {
 };
 
 // Helper function for displaying photos
-function displayPhotos(photos) {
+const displayPhotos = (photos) => {
   allImages.innerHTML = ""; // Clears existing photos on page
   for (let i = 0; i < photos.length; i++) {
   let img = document.createElement("img");
