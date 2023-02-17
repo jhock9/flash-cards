5fe228b46910d40d036ff192c7829f5c88dea1ad
Jon Hocker <jon.hocker@gmail.com>
2023-02-16 23:14:24 -0600

    config cloudkit and apple sign in

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
+// TODO - things to keep track of
+// 1. Once the user is authenticated, you can use CloudKit's JavaScript API to access their iCloud Photos. 
+// 2. You need to make sure that your CloudKit container is set up to allow access to the user's iCloud Photos, 
+//    and that your web app has the necessary permissions to access the photos.
+// 3. Use the CloudKit API to query the database and retrieve the metadata for the photos you want to display in your app.
+// 4. Use the CloudKit API to download the photos and display them in your web app.
+// 5. Once finished, split code into separate .js files (app.js and cloudkit.js) and use webpack to bundle them together.
+
 "use strict";
-// require('./main.css');
-// if (typeof window !== 'undefined' && window.process && window.process.env && window.process.env.NODE_ENV === 'production') {
+require('dotenv').config({ path: '../.env' });
+// const CloudKit = require('../cloudkit');
+const appleAuth = require('../apple-auth');
+const appleSignin = require("../apple-signin-auth");
 
 const flashCardPage = document.querySelector("#flash-card-page");
 const landingPage = document.querySelector("#landing-page");
-const signInBtn = document.querySelector("#apple-sign-in-button");// need this still??
+// const signInBtn = document.querySelector("#apple-sign-in-button");// need this still??
 const submit = document.querySelector('#submit-btn');
 const filterList = document.querySelector('.filter-list');
 const filterInputs = Array.from(document.getElementsByClassName('qty'));
@@ -17,122 +27,113 @@ console.log(process.env.ICLOUD_CONTAINER);
 console.log(process.env.ICLOUD_API_KEY);
 console.log(process.env.ICLOUD_REDIRECT_URI);
 
-//* CONFIGURING CLOUDKIT & ICLOUD USER AUTHENTICATION
-// window.addEventListener('cloudkitloaded', () => {
-  console.log('listening for cloudkitloaded');
-  CloudKit.configure ({
-    containers: [{
-      containerIdentifier: process.env.ICLOUD_CONTAINER,
-      apiTokenAuth: {
-          apiToken: process.env.ICLOUD_API_KEY,
-          persist: true, // keeps user signed in after closing/reopening browser
-          // useAuth: true
-          signInButton: {
-            id: 'apple-sign-in-button',
-            theme: 'black' // Other options: 'white', 'white-with-outline'.
-          },
+//* SET UP CLOUDKIT
+CloudKit.configure ({
+  containers: [{
+    containerIdentifier: process.env.ICLOUD_CONTAINER,
+    apiTokenAuth: {
+        apiToken: process.env.ICLOUD_API_KEY,
+        persist: true, // creates a cookie that can be used to re-authenticate the user
+        serverToServerKeyAuth: {
+          keyID: process.env.APPLE_KEY_ID,
+          privateKeyFile: '../VBMAPP-AuthKey_5V3RA3367C.p8'
         },
-      environment: process.env.NODE_ENV === 'production' ? 'production' : 'development'
-    }]
+      },
+    environment: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+  }]
+});
+
+const container = CloudKit.getDefaultContainer();
+const publicDB = CloudKit.getDefaultContainer().publicCloudDatabase;
+const privateDB = CloudKit.getDefaultContainer().privateCloudDatabase;
+
+//* SIGN IN WITH APPLE
+const signInApple = async () => {
+  const response = await window.AppleID.auth.signIn();
+  console.log("signInApple: " + response);
+  const user = response.user;
+  const authorization = response.authorization;
+  const token = authorization.id_token;
+  try {
+    const { sub: userAppleId } = await appleSignin.verifyIdToken(token, {
+      audience: process.env.ICLOUD_CONTAINER,
+      ignoreExpiration: true,
+    });
+    console.log("User ID: ", userAppleId);
+    handleSignInWithApple(authorization);
+  } catch (err) {
+    console.error("Error verifying Apple ID token: ", err);
+  }
+  return response;  
+};
+
+document.addEventListener('AppleIDSignInOnSuccess', (event) => {
+  console.log("Apple ID sign in successful: ", event.detail.data);
+});
+
+document.addEventListener('AppleIDSignInOnFailure', (event) => {
+  console.log("Apple ID sign in failed: ", event.detail.error);
+});
+
+// Configure the Apple auth object with your app's configuration
+const auth = new appleAuth({
+  clientId: process.env.ICLOUD_CONTAINER,
+  teamId: process.env.APPLE_TEAM_ID,
+  keyId: process.env.APPLE_KEY_ID,
+  privateKeyPath: '../VBMAPP-AuthKey_5V3RA3367C.p8',
+});
+
+const handleSignInWithApple = async (authorization) => {
+  try {
+    // Exchange the authorization code for an access token and ID token
+    const { access_token, id_token } = await auth.accessToken(authorization.code);
+    // Verify the ID token using the apple-auth library
+    const { sub: userAppleId } = await appleSignin.verifyIdToken(id_token, {
+      audience: process.env.ICLOUD_CONTAINER,
+      ignoreExpiration: true,
+    });
+    console.log("User ID: ", userAppleId); // Now can fetch photos from iCloud
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Query the database for photos
+const query = { /* your query here */ };
+publicDB.performQuery(query)
+  .then((response) => {
+    // Handle the response
+  })
+  .catch((error) => {
+    // Handle the error
   });
-  console.log('cloudkitloaded');
 
-  let container = CloudKit.getDefaultContainer();
-  container.setUpAuth().then((userInfo) => {
-    if(userInfo) {
-      // this.gotoAuthenticatedState(userInfo);
-      console.log(userInfo + " " + "is authenticated.");
-    } else {
-      // this.gotoUnauthenticatedState();
-      console.log(userInfo + " " + "is not authenticated.");
-    }
-  });
-
-  container.whenUserSignsIn().then((userInfo) => {
-    console.log(userInfo + " " + "just signed in.");
-  });
-  container.whenUserSignsOut().then((userInfo) => {
-    console.log(userInfo + " " + "just signed out.");
-  });
-
-
-  // let TILViewModel = () => {
-  //   console.log("get default container");
-  //   let container = CloudKit.getDefaultContainer();
-    
-  //   console.log("set publicDB");
-  //   let publicDB = container.publicCloudDatabase;
-  //   this.items = ko.observableArray();
-
-  //   // Fetch public records
-  //   this.fetchRecords = () => {
-  //     console.log("fetching records from " + publicDB);
-  //     let query = { recordType: 'Acronym', sortBy: [{ fieldName: 'short'}] };
-      
-  //     // Execute the query.
-  //     return publicDB.performQuery(query).then((response) => {
-  //       if (response.hasErrors) {
-  //         console.error(response.errors[0]);
-  //         return;
-  //       }
-  //       let records = response.records;
-  //       let numberOfRecords = records.length;
-  //       if (numberOfRecords === 0) {
-  //         console.error('No matching items');
-  //         return;
-  //       }
-  //       this.items(records);
-  //     });
-  //   };
-
-  //   this.displayUserName = ko.observable('Unauthenticated User');
-  //   this.gotoAuthenticatedState = (userInfo) => {
-  //     if(userInfo.isDiscoverable) {
-  //       this.displayUserName(userInfo.firstName + ' ' + userInfo.lastName);
-  //     } else {
-  //       this.displayUserName('userInfo.userRecordName');
-  //     }
-    
-  //     container
-  //     .whenUserSignsOut()
-  //     .then(this.gotoUnauthenticatedState);
-  //   };
-    
-  //   this.gotoUnauthenticatedState = (error) => {
-  //     this.displayUserName('Unauthenticated User');
-    
-  //     container
-  //     .whenUserSignsIn()
-  //     .then(this.gotoAuthenticatedState)
-  //     .catch(this.gotoUnauthenticatedState);
-  //   };
-    
-  
-  //   container.setUpAuth().then((userInfo) => {
-  //     console.log("setUpAuth");
-  //     this.fetchRecords(); // Don't need user auth to fetch public records
-    
-  //     if (userInfo) {
-  //       this.gotoAuthenticatedState(userInfo);
-  //     } else {
-  //       this.gotoUnauthenticatedState();
-  //     }
-  //   });
-  // }
-  
-  // ko.applyBindings(new TILViewModel());
+// container.setUpAuth().then((userInfo) => {
+//   if(userInfo) {
+//     // this.gotoAuthenticatedState(userInfo);
+//     console.log(userInfo + " " + "is authenticated.");
+//   } else {
+//     // this.gotoUnauthenticatedState();
+//     console.log(userInfo + " " + "is not authenticated.");
+//   }
 // });
 
+// container.whenUserSignsIn().then((userInfo) => {
+//   console.log(userInfo + " " + "just signed in.");
+// });
+// container.whenUserSignsOut().then((userInfo) => {
+//   console.log(userInfo + " " + "just signed out.");
+// });
 
 
 // CloudKit.getAuthStatus().then((response) => {
 //   if (response.status === 'AUTHORIZED') {
-//     console.log('User is already signed in');
+//     console.log("User is already signed in.");
 //     landingPage.classList.add("hide");
 //     flashCardPage.classList.remove("hide");
 //     fetchAlbums();
 //   } else {
-//     console.log('User is not signed in');
+//     console.log("User is not signed in.");
 //     signInBtn.addEventListener("click", () => {
 //       CloudKit.signIn({
 //         scope: 'email',
