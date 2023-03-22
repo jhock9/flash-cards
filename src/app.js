aa830077874bcc910f31103b00e451b962a1b5a5
Jon Hocker <jon.hocker@gmail.com>
2023-03-22 13:13:39 -0500

    tweaking Sign-In with Apple

--- src/app.js
+++ src/app.js
@@ -22,41 +22,22 @@ let userIdentity = null;
 
 console.log(process.env.NODE_ENV + " AND ALL other env variables logging out");
 
-//* SET UP CLOUDKIT
-CloudKit.configure ({
-  containers: [{
-    containerIdentifier: process.env.ICLOUD_CONTAINER,
-    apiTokenAuth: {
-        apiToken: process.env.ICLOUD_API_KEY,
-        persist: true, // creates a cookie that can be used to re-authenticate the user
-        serverToServerKeyAuth: {
-          keyID: process.env.APPLE_KEY_ID,
-          privateKeyFile: '../AuthKey.p8',
-        },
-      },
-    environment: process.env.NODE_ENV === 'production' ? 'production' : 'development'
-  }]
-});
-
-const container = CloudKit.getDefaultContainer();
-const publicDB = CloudKit.getDefaultContainer().publicCloudDatabase;
-const privateDB = CloudKit.getDefaultContainer().privateCloudDatabase;
-
 //* INITIALIZE SIGN IN WITH APPLE
-const initApple = () => {
-  window.AppleID.auth.init({
+// const handleAppleSignIn = () => {
+  AppleID.auth.init({
     clientId: process.env.APPLE_CLIENT_ID,
     redirectURI: process.env.CLOUD_REDIRECT_URI,
     usePopup: true
   });
-}
+  // AppleID.auth.signIn();
+// }
 
 // start authentication flow
-const signInApple = async () => {
-  const response = await window.AppleID.auth.signIn();
-  console.log("signInApple: " + response);
-  return response;
-};
+// const signInApple = async () => {
+//   const response = await AppleID.auth.signIn();
+//   console.log("signInApple: " + response);
+//   return response;
+// };
 
 // Listen for sign in success or failure
 document.addEventListener('AppleIDSignInOnSuccess', (event) => {
@@ -66,15 +47,39 @@ document.addEventListener('AppleIDSignInOnFailure', (event) => {
   console.log("Apple ID sign in failed: ", event.detail.error);
 });
 
-// Query the database for photos
-const query = { /* your query here */ };
-publicDB.performQuery(query)
-  .then((response) => {
-    // Handle the response
-  })
-  .catch((error) => {
-    // Handle the error
+
+//* SET UP CLOUDKIT
+async function fetchAlbums() {
+  CloudKit.configure ({
+    containers: [{
+      containerIdentifier: process.env.ICLOUD_CONTAINER,
+      apiTokenAuth: {
+          apiToken: process.env.ICLOUD_API_KEY,
+          persist: true, // creates a cookie that can be used to re-authenticate the user
+          serverToServerKeyAuth: {
+            keyID: process.env.APPLE_KEY_ID,
+            privateKeyFile: '../AuthKey.p8',
+          },
+        },
+      environment: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+    }]
   });
+
+  const container = CloudKit.getDefaultContainer();
+  const publicDB = container.publicCloudDatabase;
+  const privateDB = container.privateCloudDatabase;
+
+  // Fetch albums
+  const query = { recordType: 'Album' };
+  const response = await database.performQuery(query);
+
+  if (response.hasErrors) {
+    console.error('Error fetching albums:', response.errors[0]);
+    return [];
+  }
+
+  return response.records.map(record => record.fields.name.value);
+}
 
 // container.setUpAuth().then((userInfo) => {
 //   if(userInfo) {
