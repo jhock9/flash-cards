e1ede0a0fdf2088ad58d5cafd004edfa88856c5c
Jon Hocker <jon.hocker@gmail.com>
2023-03-25 13:15:16 -0500

    removing unnecessary packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/app.js',
@@ -11,10 +10,7 @@ module.exports = {
     clean: true,
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    // publicPath: 'dist/',
-    // sourceMapFilename: 'bundle.js.map'
   },
-  // devtool: 'source-map',
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: {loader: 'babel-loader', options: { presets: ['@babel/preset-env']}}},
@@ -22,7 +18,6 @@ module.exports = {
       { test: /\.s(a|c)ss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
       { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource', generator: { filename: 'assets/[name][ext]' }},
       { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource', generator: { filename: 'assets/[name][ext]' }},
-      // { mimetype: 'application/javascript', type: 'js',}, 
     ]
   },
   resolve: {
@@ -50,23 +45,10 @@ module.exports = {
         return resource.startsWith('fs') && process.env.NODE_ENV === 'production';
       },
     }),
-    new Dotenv({
-      path: path.resolve(__dirname, '.env'),
-      systemvars: true
-    }),
   ],
   devServer: {
     static: path.resolve('./'),
     open: true,
     port: 3003,
-    // historyApiFallback: true,
-    // devMiddleware: {
-    //   mimeTypes: { 
-    //     "js": "application/javascript",
-    //     "html": "text/html",
-    //     "css": "text/css",
-    //     "png": "image/png"
-    //   },
-    // },
   },
-}
+};
