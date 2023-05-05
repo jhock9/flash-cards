d5e1ac1fdf352154b8fc831e7326ebb11cdfec29
Jon Hocker <jon.hocker@gmail.com>
2023-05-04 21:56:28 -0500

    updating auth process, added photos api call

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const API_KEY = process.env.GOOGLE_API_KEY;
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const REDIRECT_URL = process.env.REDIRECT_URL;
-// console.log('REDIRECT_URL:', REDIRECT_URL);
 
 // Serve static files
 app.use(express.static(path.join(__dirname, '../src/')));
@@ -26,16 +25,16 @@ app.get('/config', (req, res) => {
 
 //* Obtaining OAuth 2.0 access tokens
 // Import the Google Auth and Google APIs libraries
-// const { google } = require('googleapis');
+const { google } = require('googleapis');
 // // const photoslibrary = google.photoslibrary('v1');
 
-// // Set up your OAuth2 client for the API
-// const oauth2Client = new google.auth.OAuth2(
-//   CLIENT_ID,
-//   CLIENT_SECRET,
-//   REDIRECT_URL,
-// );
-// console.log('OAuth2 client created with redirect URL:', oauth2Client.redirectUri);
+// Set up your OAuth2 client for the API
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL,
+);
+console.log('OAuth2 client CREATED.');
 
 // // Generate a url that asks permissions for the scope
 // const authorizationUrl = oauth2Client.generateAuthUrl({
@@ -77,40 +76,57 @@ app.get('/config', (req, res) => {
 //     }
 //   })();
 // });
-// //!! this is where it falls apart
 
 // // Redirect to Google's OAuth 2.0 server
-//   console.log(`Redirecting to Google's OAuth 2.0 server:`, authorizationUrl);
-//   res.writeHead(301, { "Location": authorizationUrl });
+  // console.log(`Redirecting to Google's OAuth 2.0 server:`, authorizationUrl);
+  // res.writeHead(301, { "Location": authorizationUrl });
 
 // Exchange authorization code for refresh and access tokens
-const url = require('url');
+// const url = require('url');
 
-console.log('Handling the OAuth 2.0 server response');
-if (req.url.startsWith('/oauth2callback')) {
-  // Handle the OAuth 2.0 server response
-  let q = url.parse(req.url, true).query;
-  console.log('Received query:', q);
+// console.log('HANDLING OAuth 2.0 server response');
+// if (req.url.startsWith('/oauth2callback')) {
+//   // Handle the OAuth 2.0 server response
+//   let q = url.parse(req.url, true).query;
+//   console.log('Received query:', q);
 
-  // Get access and refresh tokens (if access_type is offline)
-  let { tokens } = await oauth2Client.getToken(q.code);
-  console.log('Received tokens:', tokens);
-  oauth2Client.setCredentials(tokens);
-  console.log('Credentials set for the OAuth2 client');
-}
+//   // Get access and refresh tokens (if access_type is offline)
+//   let { tokens } = await oauth2Client.getToken(q.code);
+//   console.log('Tokens RECEIVED.', tokens);
+//   oauth2Client.setCredentials(tokens);
+//   console.log('OAuth2 client credentials SET.');
+// }
+// //!! this is where it falls apart
 
-// // Server-side endpoint for fetching albums from Google Photos API
-// app.get('/api/albums', async (req, res) => {
-//   try {
-//     const response = await photoslibrary.albums.list({
-//       auth: oauth2Client
-//     });
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error('Error fetching albums:', error);
-//     res.status(500).json({ status: 'failure', message: 'Failed to fetch albums' });
-//   }
-// });
+app.post('/oauth2callback', express.json(), async (req, res) => {
+  console.log('HANDLING OAuth 2.0 server response');
+  try {
+    const code = req.body.code;
+    const { tokens } = await oauth2Client.getToken(code);
+    oauth2Client.setCredentials(tokens);
+    res.status(200).send('Tokens obtained.');
+    console.log('Tokens RECEIVED.', tokens);
+  } catch (error) {
+    res.status(500).send('ERROR exchanging authorization code.');
+    console.error('ERROR exchanging authorization code:', error);
+  }
+});
+
+// Server-side endpoint for fetching albums from Google Photos API
+const photos = google.photos({version: 'v1', auth: oauth2Client});
+
+app.get('/api/list-albums', async (req, res) => {
+  try {
+    const response = await photos.albums.list({
+      auth: oauth2Client,
+    });
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error('Error calling Google Photos API:', error);
+    res.status(500).send('Error calling Google Photos API.');
+  }
+});
 
 // Log serving file
 app.use((req, res, next) => {
