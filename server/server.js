e8959e26a84bb9bb1c766b294d86afda883af493
Jon Hocker <jon.hocker@gmail.com>
2023-05-06 22:50:33 -0500

    trying axios again

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
-// const axios = require('axios');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3003;
 
@@ -53,13 +53,16 @@ app.post('/oauth2callback', jsonParser, async (req, res) => {
   }
 });
 
-// Fetch albums from Google Photos API
-const photosLibraryApi = google.photoslibrary('v1');
 
+// Fetch albums from Google Photos API
 app.get('/api/list-albums', async (req, res) => {
   try {
-    const response = await photosLibraryApi.albums.list({
-      auth: oauth2Client,
+    const url = 'https://photoslibrary.googleapis.com/v1/albums';
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${oauth2Client.credentials.access_token}`,
+        'Content-Type': 'application/json',
+      },
     });
     res.json(response.data);
   } catch (error) {
