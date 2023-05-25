81719b607f1e3755d37223b52e4ed2dc1677ad8a
Jon Hocker <jon.hocker@gmail.com>
2023-05-25 13:33:54 -0500

    removed axios package

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3003;
 const {google} = require('googleapis');
@@ -87,24 +86,6 @@ app.post('/oauth2callback', async (req, res) => {
   } catch (error) {
     console.error('ERROR exchanging authorization code:', error);
     res.status(500).json({ success: false, error: error.toString() });
-  }
-});
-
-// Fetch albums from Google Photos API
-app.get('/api/list-albums', async (req, res) => {
-  try {
-    const url = 'https://photoslibrary.googleapis.com/v1/albums';
-    const accessToken = await oauth2Client.getAccessToken();
-    const response = await axios.get(url, {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
-      },
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error fetching albums');
   }
 });
 
