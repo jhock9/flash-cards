1eb820fea73f48e5be840960275a0a2f4454ba8f
Jon Hocker <jon.hocker@gmail.com>
2023-09-17 23:54:49 -0500

    clearing typos

--- server/morgan.js
+++ server/morgan.js
@@ -1,21 +1,21 @@
 const morgan = require("morgan");
 const logger = require("./winston.js");
-​
+
 const stream = {
   // Use the http severity
   write: (message) => logger.http(message),
 };
-​
+
 const skip = () => {
   const env = process.env.NODE_ENV || "development";
   return env !== "development";
 };
-​
+
 const morganMiddleware = morgan(
   // Define message format string (this is the default one).
   ":remote-addr :method :url :status :res[content-length] - :response-time ms",
   // Options: in this case, I overwrote the stream and the skip logic.
   { stream, skip }
 );
-​
+
 module.exports = morganMiddleware;
--- server/winston.js
+++ server/winston.js
@@ -8,14 +8,14 @@ const levels = {
   http: 3,
   debug: 4,
 }
-​
+
 // This method set the current severity based on the current NODE_ENV 
 const level = () => {
   const env = process.env.NODE_ENV || 'development'
   const isDevelopment = env === 'development'
   return isDevelopment ? 'debug' : 'warn'
 }
-​
+
 // Define different colors for each level.
 const colors = {
   error: 'red',
@@ -24,10 +24,10 @@ const colors = {
   http: 'magenta',
   debug: 'white',
 }
-​
+
 // Link colors defined above to the severity levels.
 winston.addColors(colors)
-​
+
 // Chose the aspect of your log customizing the log format.
 const format = winston.format.combine(
   winston.format.timestamp({ format: 'YYYY-MM-DD hh:mm:ss:ms A' }),
@@ -37,14 +37,14 @@ const format = winston.format.combine(
     (info) => `${info.timestamp} ${info.level}: ${info.message}`,
   ),
 )
-​
+
 // Define which transports the logger must use to print out messages.
 const transports = [
   new winston.transports.Console(),
   new winston.transports.File({ filename: './logs/error.log', level: 'error', }),
   new winston.transports.File({ filename: './logs/all.log' }),
 ]
-​
+
 // Create the logger instance that has to be exported and used to log messages.
 const logger = winston.createLogger({
   level: level(),
@@ -52,5 +52,5 @@ const logger = winston.createLogger({
   format,
   transports,
 })
-​
+
 module.exports = logger
