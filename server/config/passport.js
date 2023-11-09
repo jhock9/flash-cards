673c47e40e0a442c1ca624a3c849690293fd0c13
Jon Hocker <jon.hocker@gmail.com>
2023-11-09 14:54:55 -0600

    updated unsupported deserializeUser method

--- server/config/passport.js
+++ server/config/passport.js
@@ -20,10 +20,13 @@ const localPassport = (passport) => {
     done(null, user.id);
   });
   
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-      done(err, user);
-    });
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
   });
 };
 
