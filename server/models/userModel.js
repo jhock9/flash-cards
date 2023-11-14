f6c997e81697249ecba25a43e177866f82678dfc
Jon Hocker <jon.hocker@gmail.com>
2023-11-13 22:53:48 -0600

    updating models

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-});
+  favoriteTags: [{ type: String }],
+}, { timestamps: true });
 
 // Password hashing middleware
 userSchema.pre('save', async function(next) {
