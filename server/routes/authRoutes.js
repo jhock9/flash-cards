f0c6b46ed353a8bbd72c53ddae17f6287e9d8483
Jon Hocker <jon.hocker@gmail.com>
2023-12-05 11:54:02 -0600

    debug req.logOut callback

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -71,7 +71,7 @@ router.post('/login', (req, res, next) => {
 // Logout route
 router.get('/logout', (req, res) => {
   console.log('Logging out user, req.logOut:', req.logOut);
-  req.logOut();
+  req.logOut(() => {});
   req.session.destroy(() => {
     console.log('User logged out.');
     res.status(200).json({ message: 'Logged out' });
