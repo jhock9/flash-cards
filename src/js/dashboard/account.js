ce341f40bf5e46e5c303ef769d1e126afce39e8b
Jon Hocker <jon.hocker@gmail.com>
2023-12-12 16:26:13 -0600

    Add user account details to dashboard

--- src/js/dashboard/account.js
+++ src/js/dashboard/account.js
@@ -6,10 +6,21 @@ const fetchAccountData = async () => {
       throw new Error(`Server responded with status: ${response.status}`);
     }
     const data = await response.json();
+    
+    const acctName = document.getElementById('acct-name');
+    const acctUsername = document.getElementById('acct-username');
+    const acctPassword = document.getElementById('acct-password');
+    if (data.user) {
+      if (acctName) acctName.textContent = data.user.name;
+      if (acctUsername) acctUsername.textContent = data.user.username;
+      if (acctPassword) acctPassword.textContent = data.user.password;
+    }
+    
     return data.user;
   } catch (error) {
     console.error('Error fetching user data:', error);
   }
 };
 
+// Export to dashboard.js
 export { fetchAccountData };
