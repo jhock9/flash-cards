93e60c2f77bb9f1b60aa29a8409036f89bb6647c
Jon Hocker <jon.hocker@gmail.com>
2023-12-12 21:58:31 -0600

    added logging for debugging

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -3,6 +3,8 @@ const User = require('../models/userModel');
 
 router.get('/account-data', async (req, res) => {
   try {
+    logger.debug('Session data:', req.session); // Log the session data
+
     if (!req.session.user.username) {
       return res.status(401).json({ message: 'Not authenticated' });
     }
@@ -14,7 +16,8 @@ router.get('/account-data', async (req, res) => {
     
     res.json({ user });
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    logger.debug('Error in /account-data route:', error)
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 });
 
--- src/js/dashboard/account.js
+++ src/js/dashboard/account.js
@@ -2,11 +2,13 @@
 const fetchAccountData = async () => {
   try {
     const response = await fetch('/account/account-data', { credentials: 'include' });
+    console.log('Response:', response); // Log the entire response object
     if (!response.ok) {
       throw new Error(`Server responded with status: ${response.status}`);
     }
     const data = await response.json();
-    
+    console.log('Data:', data); // Log the data returned by the server
+
     const acctName = document.getElementById('acct-name');
     const acctUsername = document.getElementById('acct-username');
     const acctPassword = document.getElementById('acct-password');
