3d25dee5307e71fdd130e07b2ca8030154510afc
Jon Hocker <jon.hocker@gmail.com>
2023-12-19 23:45:42 -0600

    Added clientID field, export

--- server/models/clientModel.js
+++ server/models/clientModel.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const clientSchema = new mongoose.Schema({
   fullname: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  clientID: { type: String, required: true },
+  
 }, { timestamps: true });
 
 const Client = mongoose.model('Client', clientSchema);
 
-//!! export to where?
+// Export to clientRoutes.js
 // Also used in sessionModel.js
 module.exports = Client;
