796223d3fd43baa2cd83a9aea70b8608ff08114b
Jon Hocker <jon.hocker@gmail.com>
2023-12-19 23:46:31 -0600

    added lockedTags, lockedPhoto

--- server/models/sessionModel.js
+++ server/models/sessionModel.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const sessionSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   client: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' },
-  savedPhotos: [{ type: String }],
+  lockedTags: [{
+    name: { type: mongoose.Schema.Types.ObjectId, ref: 'Photo'},
+    qty: { type: Number, default: 0 }, 
+    locked: { type: Boolean, default: false } 
+  }],
+  lockedPhoto: { type: mongoose.Schema.Types.ObjectId, ref: 'Photo' },
 }, { timestamps: true });
 
 const Session = mongoose.model('Session', sessionSchema);
