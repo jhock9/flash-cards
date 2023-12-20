83486a1c89db79f45f2707ce53dca4b7b0c4fcad
Jon Hocker <jon.hocker@gmail.com>
2023-12-19 23:46:14 -0600

    removed unused fields

--- server/models/photoModel.js
+++ server/models/photoModel.js
@@ -4,13 +4,6 @@ const photoSchema = new mongoose.Schema({
   googleId: { type: String, required: true, unique: true },
   baseUrl: { type: String, required: true },
   tagsFromGoogle: [{ type: String, required: true }],
-  tags: [{  //!! do I really need this?
-    name: String, 
-    qty: { type: Number, default: 0 }, 
-    locked: { type: Boolean, default: false } 
-  }],
-  isFavorite: { type: Boolean, default: false },
-  selectedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 }, { timestamps: true });
 
 photoSchema.index({ 'tags.name': 1 });
@@ -19,4 +12,5 @@ photoSchema.index({ tagsFromGoogle: 'text' });
 const Photo = mongoose.model('Photo', photoSchema);
 
 // Export to photoController.js and photoDBRoutes.js
+// Also used in sessionModel.js
 module.exports = Photo;
