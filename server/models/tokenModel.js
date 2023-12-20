e6b10968f53512734284514d876dbd31c5699516
Jon Hocker <jon.hocker@gmail.com>
2023-12-19 23:46:39 -0600

    added timestamp

--- server/models/tokenModel.js
+++ server/models/tokenModel.js
@@ -4,7 +4,7 @@ const tokenSchema = new mongoose.Schema({
   accessToken: String,
   refreshToken: String,
   isGoogleAuthenticated: { type: Boolean, default: false },
-});
+}, { timestamps: true });
 
 const Token = mongoose.model('Token', tokenSchema);
 
