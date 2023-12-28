857662de05fad692d8c73bc8d1c7670d4406eff0
Jon Hocker <jon.hocker@gmail.com>
2023-12-28 14:48:15 -0600

    Add _id field to userResponse

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -14,6 +14,7 @@ router.get('/account-data', async (req, res) => {
     }
     
     const userResponse = {
+      _id: user._id,
       fullname: user.fullname,
       username: user.username,
       role: user.role,
