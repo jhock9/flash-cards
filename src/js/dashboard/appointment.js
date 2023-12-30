d1865a68cba7acb1700d2f76e626095b8f8bec80
Jon Hocker <jon.hocker@gmail.com>
2023-12-30 12:34:48 -0600

    abstracted appointment code to new file

---
+++ src/js/dashboard/appointment.js
@@ -0,0 +1,45 @@
+const fetchAppointment = async (clientId) => {
+  try {
+    const response = await fetch(`/appointment/${clientId}`);
+    const data = await response.json();
+    
+    //!! just for debugging
+    console.log(`Server responded with status code: ${response.status}`);
+    console.log(`Response data: ${JSON.stringify(data)}`);
+    
+    if (response.status >= 400) {
+      // The server responded with an error
+      console.error(`Server responded with status code ${response.status}`);
+    }
+    
+    if (response.status === 404) {
+      console.log('No appointment found, creating new appointment...');
+      const newAppointmentData = await createAppointment(clientId);
+      window.location.href = `/flashcards?appointment=${encodeURIComponent(JSON.stringify(newAppointmentData.appointment))}`;
+    } else {
+      console.log('Appointment found...');
+      window.location.href = `/flashcards?appointment=${encodeURIComponent(JSON.stringify(data.appointment))}`;
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+const createAppointment = async (clientId) => {
+  console.log(`Sending POST request to /appointment with client ID: ${clientId}`); //!! just for debugging
+  const response = await fetch(`/appointment`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ clientId }),
+    credentials: 'include',
+  });
+  const data = await response.json();
+  console.log('New appointment created...');
+  //!! just for debugging
+  console.log(`Server responded with status code: ${response.status}`);
+  console.log(`New appointment data: ${JSON.stringify(data)}`);
+  return data;
+};
+
+// Export to client.js
+export { fetchAppointment };
--- src/js/dashboard/clients.js
+++ src/js/dashboard/clients.js
@@ -1,4 +1,5 @@
 import { fetchAccountData } from './account.js';
+import { fetchAppointment } from './appointment.js';
 import { 
   hideModal,
   showClientCreatedModal,
@@ -132,31 +133,7 @@ const createViewAppointmentsBtn = (clientId) => {
   viewAppointmentsBtn.addEventListener('click', async () => {
     console.log('viewAppointmentsBtn clicked...');
     console.log(`Fetching appointment for client with ID: ${clientId}`); //!! just for debugging
-    
-    try {
-      const response = await fetch(`/appointment/${clientId}`);
-      const data = await response.json();
-      
-      //!! just for debugging
-      console.log(`Server responded with status code: ${response.status}`);
-      console.log(`Response data: ${JSON.stringify(data)}`);
-      
-      if (response.status >= 400) {
-        // The server responded with an error
-        console.error(`Server responded with status code ${response.status}`);
-      }
-      
-      if (response.status === 404) {
-        console.log('No appointment found, creating new appointment...');
-        const newAppointmentData = await createAppointment(clientId);
-        window.location.href = `/flashcards?appointment=${encodeURIComponent(JSON.stringify(newAppointmentData.appointment))}`;
-      } else {
-        console.log('Appointment found...');
-        window.location.href = `/flashcards?appointment=${encodeURIComponent(JSON.stringify(data.appointment))}`;
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
+    await fetchAppointment(clientId);
   });
   
   return viewAppointmentsBtn;
@@ -217,22 +194,6 @@ const createClient = async (event) => {
   .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
   });
-};
-
-const createAppointment = async (clientId) => {
-  console.log(`Sending POST request to /appointment with client ID: ${clientId}`); //!! just for debugging
-  const response = await fetch(`/appointment`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ clientId }),
-    credentials: 'include',
-  });
-  const data = await response.json();
-  console.log('New appointment created...');
-  //!! just for debugging
-  console.log(`Server responded with status code: ${response.status}`);
-  console.log(`New appointment data: ${JSON.stringify(data)}`);
-  return data;
 };
 
 // Export to dashboard.js
