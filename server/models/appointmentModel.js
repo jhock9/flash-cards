8d2e1c8cd54f19c260c299f5affdaf59efeeef85
Jon Hocker <jon.hocker@gmail.com>
2024-01-01 22:25:44 -0600

    updated names for savedTags, savedPhotos

--- server/models/appointmentModel.js
+++ server/models/appointmentModel.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const appointmentSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   client: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' },
-  lockedTags: [{
+  savedTags: [{
     name: { type: mongoose.Schema.Types.ObjectId, ref: 'Photo'},
     qty: { type: Number, default: 0 }, 
     locked: { type: Boolean, default: false } 
   }],
-  lockedPhotos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Photo' }],
+  savedPhotos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Photo' }],
 }, { timestamps: true });
 
 const Appointment = mongoose.model('Appointment', appointmentSchema);
