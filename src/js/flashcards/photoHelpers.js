e1952505866c9799f755052677c7b21f3372772f
Jon Hocker <jon.hocker@gmail.com>
2024-01-23 15:10:00 -0600

    Add intendedTotal parameter to addPhotos()

--- src/js/flashcards/photoHelpers.js
+++ src/js/flashcards/photoHelpers.js
@@ -29,7 +29,7 @@ const addRemainingPhotos = (useRemainder, remainingPhotos, totalPhotos, photos, 
   return additionalPhotos.slice(0, Math.min(remainingPhotos, totalPhotos - selectedPhotoIds.size));
 };
 
-const addPhotos = (newPhotos, selectedPhotoIds, filteredPhotos, lockedPhoto) => {
+const addPhotos = (newPhotos, selectedPhotoIds, filteredPhotos, lockedPhoto, intendedTotal) => {
   newPhotos.forEach(photo => {
     // Add the photo to filteredPhotos and update selectedPhotoIds
     selectedPhotoIds.add(photo.googleId);
@@ -40,6 +40,15 @@ const addPhotos = (newPhotos, selectedPhotoIds, filteredPhotos, lockedPhoto) => 
   if (lockedPhoto && !filteredPhotos.includes(lockedPhoto)) {
     selectedPhotoIds.add(lockedPhoto.googleId);
     filteredPhotos.unshift(lockedPhoto);
+  }
+  
+  // If the total number of photos exceeds the intended total, remove a photo with the same tag as the locked photo
+  if (filteredPhotos.length > intendedTotal) {
+    const sameTagIndex = filteredPhotos.findIndex(photo => photo.tagsFromGoogle.includes(lockedPhoto.tagsFromGoogle[0]));
+    if (sameTagIndex !== -1) {
+      const removedPhoto = filteredPhotos.splice(sameTagIndex, 1)[0];
+      selectedPhotoIds.delete(removedPhoto.googleId);
+    }
   }
 };
 
