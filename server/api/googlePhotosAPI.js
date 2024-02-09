77d0c7ae262aeed60a32aba5590be1e75ffd0a35
Jon Hocker <jon.hocker@gmail.com>
2024-02-09 14:10:16 -0600

    abstracted for readibility and modular

--- server/api/googlePhotosAPI.js
+++ server/api/googlePhotosAPI.js
@@ -14,16 +14,12 @@ const fetchGooglePhotos = async (oauth2Client) => {
     let response;
     do {
       const params = {
-        pageSize: 100,
+        pageSize: 50,
         pageToken: nextPageToken,
       };
       try {
-        response = await axios.post('https://photoslibrary.googleapis.com/v1/mediaItems:search', params, {
-          headers: {
-            'Authorization': `Bearer ${oauth2Client.credentials.access_token}`,
-            'Content-Type': 'application/json',
-          },
-        });
+        // Get photos from Google Photos API
+        response = await callGooglePhotosAPI(params, oauth2Client);
         logger.info(`Received ${response.data.mediaItems.length} photos from Google Photos API in initial request`);
       } catch (error) {
         logger.error(`Error getting photos: ${error.message}`);
@@ -31,17 +27,9 @@ const fetchGooglePhotos = async (oauth2Client) => {
           logger.error(`Error response from Google Photos API: ${error.response.data}`);
           try {
             // Refresh the token
-            const newTokens = await oauth2Client.refreshAccessToken();
-            oauth2Client.setCredentials(newTokens.credentials);
-            logger.info('Tokens refreshed and set in OAuth2 client.');
-            
+            await refreshAccessToken(oauth2Client);
             // Retry the request with the new token
-            response = await axios.post('https://photoslibrary.googleapis.com/v1/mediaItems:search', params, {
-              headers: {
-                'Authorization': `Bearer ${oauth2Client.credentials.access_token}`,
-                'Content-Type': 'application/json',
-              },
-            });
+            response = await callGooglePhotosAPI(params, oauth2Client);
             logger.info(`Received ${response.data.mediaItems.length} photos from Google Photos API after refreshing token`);
           } catch (refreshError) {
             logger.error(`Failed to refresh access token: ${refreshError}`);
@@ -53,30 +41,13 @@ const fetchGooglePhotos = async (oauth2Client) => {
           logger.error(`Error getting photos: ${error.message}, Error stack: ${error.stack}`);
           throw error;
         }
-      }
-      
+      }    
       logger.info('Received media items...');
       
       // Save photo data to database
-      for (const photoData of response.data.mediaItems) {
-        if (photoData.description) { // Only process photos with a description
-          if (!photoData.id) {
-            logger.error(`Photo missing id: ${JSON.stringify(photoData)}`);
-            continue; // Skip this photo and move to the next one
-          }
-          const mappedPhotoData = {
-            googleId: photoData.id,
-            baseUrl: `${photoData.baseUrl}=w2048-h1024`,
-            tagsFromGoogle: photoData.description.split(' ').filter(Boolean),
-          };  
-          try {
-            await photoController.savePhoto(mappedPhotoData);
-          } catch (error) {
-            logger.error(`Error saving photo to database: ${error.message}, Photo data: ${JSON.stringify(mappedPhotoData)}`);
-            throw error; // This will stop the execution of fetchGooglePhotos
-          }
-        }
-      }
+      const photosToProcess = response.data.mediaItems.filter(photoData => photoData.description);
+      await Promise.all(photosToProcess.map(processPhotoData));
+      nextPageToken = response.data.nextPageToken;
     } while (nextPageToken);
     
     return response ? response.data.mediaItems : [];
@@ -84,6 +55,39 @@ const fetchGooglePhotos = async (oauth2Client) => {
     logger.error(`ERROR getting photos: ${error.message}`);
     logger.error(`Stack trace: ${error.stack}`);
     throw new Error('Failed to fetch Google Photos');
+  }
+};
+
+const callGooglePhotosAPI = async (params, oauth2Client) => {
+  return await axios.post('https://photoslibrary.googleapis.com/v1/mediaItems:search', params, {
+    headers: {
+      'Authorization': `Bearer ${oauth2Client.credentials.access_token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+const refreshAccessToken = async (oauth2Client) => {
+  const newTokens = await oauth2Client.refreshAccessToken();
+  oauth2Client.setCredentials(newTokens.credentials);
+  logger.info('Tokens refreshed and set in OAuth2 client.');
+};
+
+const processPhotoData = async (photoData) => {
+  if (!photoData.id) {
+    logger.error(`Photo missing id: ${JSON.stringify(photoData)}`);
+    return;
+  }
+  const mappedPhotoData = {
+    googleId: photoData.id,
+    baseUrl: `${photoData.baseUrl}=w2048-h1024`,
+    tagsFromGoogle: photoData.description.split(' ').filter(Boolean),
+  };
+  try {
+    await photoController.savePhoto(mappedPhotoData);
+  } catch (error) {
+    logger.error(`Error saving photo to database: ${error.message}, Photo data: ${JSON.stringify(mappedPhotoData)}`);
+    throw error;
   }
 };
 
