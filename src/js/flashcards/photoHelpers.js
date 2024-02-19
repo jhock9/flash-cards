8bd734059292dbd53944372afbfb6e704a6feb93
Jon Hocker <jon.hocker@gmail.com>
2024-02-19 17:19:18 -0600

    debugging filterPhotosByTags total slider

--- src/js/flashcards/photoHelpers.js
+++ src/js/flashcards/photoHelpers.js
@@ -55,14 +55,14 @@ const addPhotos = (photosToAdd, selectedPhotoIds, filteredPhotos, lockedPhoto, i
     }
   }
   
-  // If the total number of photos exceeds the intended total, remove a photo
-  if (filteredPhotos.length > intendedTotal) {
-    const removedPhoto = filteredPhotos.pop();
-    selectedPhotoIds.delete(removedPhoto._id);
-  }
-  console.log('Number of photos to add 3:', photosToAdd.length);
-  console.log('Number of selected photos IDs 3:', selectedPhotoIds.size);
-  console.log('Number of filtered photos after adding 3:', filteredPhotos.length);
+  // // If the total number of photos exceeds the total number of photos to be displayed
+  // while (filteredPhotos.length > totalPhotos) {
+  //   const removedPhoto = filteredPhotos.pop();
+  //   selectedPhotoIds.delete(removedPhoto._id);
+  // }
+  // console.log('Number of photos to add 3:', photosToAdd.length);
+  // console.log('Number of selected photos IDs 3:', selectedPhotoIds.size);
+  // console.log('Number of filtered photos after adding 3:', filteredPhotos.length);
 };
 
 // Add remaining photos if 'useRemainder' is checked
