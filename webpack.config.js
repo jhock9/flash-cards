cc32c9ebe4e3a0bea815c9443e49266af84a46d4
Jon Hocker <jon.hocker@gmail.com>
2024-02-20 21:50:35 -0600

    Update dependencies and environment configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const dotenv = require('dotenv').config().parsed;
 
 module.exports = {
   entry: {
